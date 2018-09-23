01dc023f11fa731f3af579215d6add57e6b6a404
alan1998 <alan_drake@btinternet.com>
2018-09-23 15:00:56 +0100

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/firebase.app.module';
-import { AngularFirestoreModule, AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs/Observable';
+import { AngularFirestoreModule, 
+          AngularFirestore, 
+          AngularFirestoreCollection, 
+          AngularFirestoreDocument
+        } from '@angular/fire/firestore';
+//import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 export interface Course {
-  name : string;
+  Name : string;
+  Boundary : any;
 }
 
 @Component({
@@ -14,18 +20,32 @@ export interface Course {
 })
 export class AppComponent {
   title = 'app';
-  coursesCollRef: AngularFirestoreCollection<Course>;
-
-
-  constructor( private aft: AngularFirestore){
-      this.coursesCollRef = this.aft.collection<Course>('Courses');
-      
+  coll_endpoint : string  ='Courses';
+  coursesColl: AngularFirestoreCollection<Course>;
+  items : Observable<Course[]>;
+  itemObjs : Observable<any[]>;
+  nas : string[] = [] ;
+  /* Later change ctor to database
+  and get objects
+  how does ctor get called ?
+  see info on setup proj for database
+  and is it evene applySourceSpanToExpressionIfNeeded. Get interface update etc
+  how to do array on interface to match firestore array type */
+  constructor( private db: AngularFirestore){
+    this.coursesColl = db.collection<Course>(this.coll_endpoint);
   }
   ngOnInit(){
-    
-    //const collection: AngularFirestoreCollection<Item> = aft.collection('items')
-
-    //collection.update(data)
-    //collection.delete()
-  }
+    //this.items = this.db.collection<Course>(this.coll_endpoint).valueChanges();     
+    this.items = this.coursesColl.valueChanges();
+    let c : AngularFirestoreDocument<Course> = this.db.doc<Course>('Courses/Course1');
+     
+    this.items.forEach(c => {
+      var pts = c[0].Name ;
+      //var obj = c[0].Boundary;
+      console.log(pts);
+      this.nas.push(pts);
+    })
+    //this.itemObjs = this.db.
+    //console.log(c.snapshotChanges.);
+    }
 }
